89fa1d1362dc5a1927bc699c9241b6e22a2e4f18
AndreiLhu <leahu.andreilucian@gmail.com>
2023-01-19 15:44:44 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import socketIOClient from 'socket.io-client';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const socket = socketIOClient('http://localhost:3001');
+  socket.on('initialMessageList', (messages) => {});
+
+  return <div className="App">Hello</div>;
 }
 
 export default App;
